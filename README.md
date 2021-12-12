34d1aab6c702db0e8266bf00a0d20ccfdabef60c
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-12-12 16:20:12 -0700

    repo clone

---
+++ README.md
@@ -0,0 +1,27 @@
+# Brew Hops!
+## Description
+Brew Hops! Is an interactive map and directory created to help you find the best route between breweries in Salt Lake City. Whether you’re a visitor or a local, this app will help you sample Salt Lake’s finest craft beer selection while keeping you safe and speedy.
+
+We were motivated to create a web application to find a route between breweries by the annual event ‘Utah Ale Trail’. We would like to find the easiest route between breweries so that multiple locations could be visited within a few days with ease and safety. We could then fully participate in the Utah Ale Trail and win prizes and merchandise.
+
+We both learned a lot about actually implementing the languages we’ve learned so far including Bootstrap, JQuery, JavaScript, HTML, and CSS.
+Working with the API documentation was challenging but we both feel more capable to read, use, and work with very different sorts of API’s.
+We are proud of the way it looks and functions. It’s a fun app!
+
+
+## Usage
+When visiting the Brew Hops! Website the user is presented with a list of the breweries on the left along with a marker for each brewery on the map located on the right of the page. The user inputs their current address in the top input bar and then selects the breweries they would like to visit. The map then populates a route beginning at the user’s starting point. 
+    ![alt text](assets/images/brewhops!.jpg)
+## Credits
+Jason Wisniewski - https://github.com/JasonWisniewski
+Sabriel Gee - https://github.com/sabrielg
+## License
+MIT License
+
+Copyright (c) 2021 Sabriel Gee
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ assets/script.js
@@ -0,0 +1,184 @@
+var currentAddress = document.querySelector("#userAddress");
+var searchButton = document.querySelector("#search-button");
+var routeBtn = document.getElementById("route-btn");
+var breweryData = [];
+var resultsLat;
+var resultsStreet;
+var startingPoint;
+var brewLatLonArray = [];
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  console.log(event);
+    startingPoint = currentAddress.value;
+    if (startingPoint.trim()=="") {
+      console.log("startingPoint")
+      var addressDiv=document.getElementById("address-verify");
+      addressDiv.innerHTML= `<div class="alert alert-dismissible fade show" role="alert">
+      <strong>Holy guacamole!</strong> We need a starting address from you!
+      <button type="button" class="btn-X" data-bs-dismiss="alert" aria-label="Close">X</button>
+    </div>`;
+      return;
+    }
+  console.log("startingPoint var", startingPoint);
+  // getMapQuest(startingPoint);
+  addressToLatLon(startingPoint,true);
+//   directions(latLon, startingPoint);
+};
+
+var addressToLatLon = function (startingPoint, user) {
+  console.log("startingPoint", startingPoint);
+  var geoCodeApi = `http://www.mapquestapi.com/geocoding/v1/address?key=rpAvJfYmOqPswEf5T36Wqk8vDHDZDa4v&location=${startingPoint}`;
+  if (!user) {
+    let promises =[];
+    for (let i = 0; i < resultsStreet.length; i++) {
+      var geoStreetApi = `http://www.mapquestapi.com/geocoding/v1/address?key=rpAvJfYmOqPswEf5T36Wqk8vDHDZDa4v&location=${resultsStreet[i].street} ${resultsStreet[i].city} ,${resultsStreet[i].state}`;
+      promises.push(fetch(geoStreetApi).then(async function (response) {
+        if (response.ok) {
+          await response.json().then(function (data) {
+            resultsStreet[i].latitude =
+              data.results[0].locations[0].latLng.lat.toString();
+            resultsStreet[i].longitude =
+              data.results[0].locations[0].latLng.lng.toString();
+          });
+        }
+      }));
+    }
+    Promise.all(promises).then(function (){
+        resultsLat = resultsLat.concat(resultsStreet);
+        loadBreweryButtons(resultsLat);
+        // chosenBrewery(resultsLat);
+    })
+
+  } else {
+    fetch(geoCodeApi).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          console.log(data.results[0].locations[0].latLng.lat);
+          console.log(data.results[0].locations[0].latLng.lng);
+          userAddressMarker(data);
+        });
+      }
+    });
+  }
+};
+
+var userAddressMarker = function (data) {
+  console.log(data);
+  var startingLat = data.results[0].locations[0].latLng.lat;
+  var startingLon = data.results[0].locations[0].latLng.lng;
+  var userIcon = L.icon({
+    iconUrl: "home.png",
+    iconSize: [50, 32],
+    iconAnchor: [25, 32],
+  });
+  console.log(startingLat);
+  L.marker([startingLat, startingLon], { icon: userIcon }).addTo(breweryMap);
+};
+
+var getBreweries = function (breweryName) {
+  var listBreweriesApi =
+    "https://api.openbrewerydb.org/breweries?by_city=salt_lake_city&sort=name:desc";
+  fetch(listBreweriesApi).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+
+        resultsLat = data.filter(function (brewery) {
+          return brewery.latitude;
+        });
+        resultsStreet = data.filter(function (brewery) {
+          return brewery.street && brewery.latitude === null;
+        });
+        addressToLatLon();
+        console.log(resultsStreet);
+      });
+    }
+  });
+};
+getBreweries();
+
+function loadBreweryButtons(data) {
+  console.log("data is", data);
+  var btnDiv = document.getElementById("btnDiv");
+  
+  for (let i = 0; i < data.length; i++) {
+    var button = document.createElement("button");
+    button.innerText = data[i].name;
+    button.value = data[i].name;
+    button.classList.add("btn");
+    button.classList.add("barbtn");
+    button.setAttribute('latitude', data[i].latitude);
+    button.setAttribute('longitude', data[i].longitude);
+    button.addEventListener("click", chosenBrewery);
+    btnDiv.appendChild(button);
+  }
+
+  console.log("Results", data);
+  for (let i = 0; i < data.length; i++) {
+    var lat = data[i].latitude;
+    var long = data[i].longitude;
+    var marker = L.marker([lat, long]).bindPopup(data[i].name);
+    //functions for popup to disappear when user hovers in and out
+    marker.on("mouseover", function (e) {
+      this.openPopup();
+    });
+    marker.on("mouseout", function (e) {
+      this.closePopup();
+    });
+    if (marker) {
+      marker.addTo(breweryMap);
+    }
+  }
+}
+
+function chosenBrewery(event) {
+  event.target.className +=" barbtn-selected";
+  console.log(event)
+  console.log(event.target.value);
+  console.log(event.target.getAttribute('latitude'));
+  console.log(event.target.getAttribute('longitude'));
+  var lat = event.target.getAttribute('latitude');
+  var lon = event.target.getAttribute('longitude');
+  var latLon = `${lat}, ${lon}`;
+  var name = event.target.value;
+  //         getMapQuest(name);
+  console.log(name);
+  console.log(resultsLat.concat(resultsStreet));
+  // grabbing bar location from API
+  var breweryAddress = event.target;
+  console.log(breweryAddress);
+    brewLatLonArray.push(latLon);
+    console.log(brewLatLonArray);
+    //   directions(latLon, startingPoint);
+  console.log(startingPoint);
+}
+
+L.mapquest.key = "rpAvJfYmOqPswEf5T36Wqk8vDHDZDa4v";
+var breweryMap = L.mapquest.map("map", {
+  // coordinates of center of the map
+  center: [40.7608, -111.891],
+  layers: L.mapquest.tileLayer("map"),
+  zoom: 12,
+});
+
+routeBtn.addEventListener('click', function (latLon){
+  var breweriesSelected = document.getElementsByClassName("barbtn");
+  console.log(breweriesSelected)
+  for (let i = 0; i < breweriesSelected.length; i++) {
+    breweriesSelected[i].classList.remove("barbtn-selected")
+  }
+    console.log('click');
+    console.log('starting point', startingPoint);
+    console.log('latlon', brewLatLonArray);
+    L.mapquest.directions().route(
+    {
+        start: startingPoint,
+        waypoints : brewLatLonArray,
+        end: startingPoint
+        },
+    );
+    
+})
+searchButton.onclick = formSubmitHandler;
